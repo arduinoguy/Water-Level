c93ba6c9d2cf8572ee1a712463e9ff48a0c923d3
Sreedev K <sreedev@resnova.in>
2015-10-24 14:40:25 +0530

    Initial proto without power savings

---
+++ GccCppProject1/GccCppProject1/water_level_tx.cpp
@@ -0,0 +1,90 @@
+﻿
+#define F_CPU 16000000UL
+#define TX_PIN 12
+#define ADDRESS "1234567" 
+#define ON		"0000001"
+#define OFF     "0000000"
+#define BOTTOM_SENSOR 2
+#define TOP_SENSOR    3
+
+bool start_flag=false;
+bool stop_flag=false;
+
+#include <Arduino.h>
+#include "VirtualWire.h"
+
+void setup();
+void loop();
+void start();
+void stop();
+void real_start();
+void real_stop();
+
+
+
+void setup() 
+{
+  pinMode(13,1);
+  DDRD&=~((1<<PORTD2)|(1<<PORTD3));
+  PORTD|=(1<<PORTD2)|(1<<PORTD3);
+  
+  pinMode(BOTTOM_SENSOR,INPUT_PULLUP);
+  pinMode(TOP_SENSOR,INPUT_PULLUP);
+  
+  attachInterrupt(0,start,RISING);
+  attachInterrupt(1,stop,FALLING);
+  
+  vw_set_tx_pin(TX_PIN);
+  vw_setup(2000);
+  real_stop();
+  
+  if(digitalRead(BOTTOM_SENSOR)&&digitalRead(TOP_SENSOR))
+  real_start();
+  
+  
+}
+
+void loop() 
+{
+   if(start_flag)
+   real_start();
+   
+   else if(stop_flag)
+   real_stop();
+    
+}
+
+
+void start()
+{
+	 digitalWrite(13,!digitalRead(13));
+	 
+	 if(digitalRead(TOP_SENSOR))
+	 start_flag=true;
+	
+}
+
+void stop()
+{digitalWrite(13,!digitalRead(13));
+//	if(!digitalRead(BOTTOM_SENSOR))
+	stop_flag=true;
+	
+}
+
+void real_start()
+{
+	start_flag=false;
+	vw_send((uint8_t*)ADDRESS,strlen(ADDRESS));
+	vw_wait_tx();
+	vw_send((uint8_t*)ON,strlen(ON));
+	vw_wait_tx();
+}
+
+void real_stop()
+{
+	stop_flag=false;
+	vw_send((uint8_t*)ADDRESS,strlen(ADDRESS));
+	vw_wait_tx();
+	vw_send((uint8_t*)OFF,strlen(OFF));
+	vw_wait_tx();
+}
---
+++ water_level_rx/water_level_rx/Sketch.cpp
@@ -0,0 +1,59 @@
+﻿
+#define F_CPU 16000000UL
+
+#define RX_PIN  12
+#define STR_LEN  8
+#define MOTOR   11
+#define ADDRESS "1234567"
+#define ON      "0000001"
+#define OFF     "0000000"
+
+#include <Arduino.h>
+#include "VirtualWire.h"
+
+
+void setup();
+void loop();
+
+
+void setup() 
+{
+	pinMode(MOTOR,1);
+	Serial.begin(9600);
+	Serial.println("Hi");
+	vw_set_rx_pin(12);
+	vw_setup(2000);
+	vw_rx_start();
+ 
+}
+
+void loop() {
+    uint8_t rec[VW_MAX_MESSAGE_LEN];
+    uint8_t msglen=VW_MAX_MESSAGE_LEN;
+	
+	vw_wait_rx();
+	
+	if(vw_get_message(rec,&msglen))
+	{
+	   
+	   if(strcmp((char*)rec,ADDRESS)==0)
+	   {
+		   Serial.println((char*)rec);
+		   vw_wait_rx_max(5000);
+		   if(vw_get_message(rec,&msglen))
+		   {
+			   Serial.println((char*)rec);
+			   
+			   if(strcmp((char*)rec,ON)==0)
+			   digitalWrite(MOTOR,1);
+			   
+			   else if(strcmp((char*)rec,OFF)==0)
+			        digitalWrite(MOTOR,0);
+		   }
+		   
+	   }
+	   
+	}
+	
+	
+}
